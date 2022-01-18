5dcd3c5b46a4f56233cca1e9c7af4f9e0588e63e
albertomolon <alberto.molon97@gmail.com>
2022-01-18 11:29:38 +0100

    Update fermat-factoring.py

--- 2-factoring-algorithms/fermat-factoring.py
+++ 2-factoring-algorithms/fermat-factoring.py
@@ -1,18 +1,25 @@
-from math import ceil, sqrt
+#################################################
+#### Use it when 'p' and 'q' are quite close ####
+#################################################
+
+from math import ceil
+from gmpy2 import iroot, divexact
 
 
 n = int(input('Insert an odd number n: '))
 
-if n % 2 == 0:
-    print('The number inserted is not odd!')
-else:
-    x = ceil(sqrt(n))
+x = ceil(iroot(n,2)[0])
+y2 = n - pow(x,2)
+
+while True:
+    if y2 == pow(ceil(iroot(y2,2)[0]),2):     # check if x2 is an integral power (e.g. x2=125 is not!)
+        break
+    x += 1
     y2 = pow(x,2) - n
 
-    while True:
-        if y2 == pow(ceil(sqrt(y2)),2):     # check if x2 is an integral power (e.g. x2=125 is not!)
-            break
-        x += 1
-        y2 = pow(x,2) - n
+p = x - iroot(y2,2)[0]
+print('p =', p)
 
-    print(f'One factor of n = {n} is m = {int(x - sqrt(y2))}')
+q = int(divexact(n,p))
+print('q =', q)
+
