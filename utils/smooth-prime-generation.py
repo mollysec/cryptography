c743af0b943dcc7026783272f46e7ac530ab8893
albertomolon <alberto.molon97@gmail.com>
2022-03-17 13:07:55 +0100

    Add smooth-prime-generation.py

---
+++ utils/smooth-prime-generation.py
@@ -0,0 +1,37 @@
+from binascii import hexlify
+from gmpy2 import *
+
+SEED  = mpz(hexlify(os.urandom(32)).decode(), 16)
+STATE = random_state(SEED)
+
+def get_prime(state, bits):
+    return next_prime(mpz_urandomb(state, bits) | (1 << (bits - 1)))
+
+def get_smooth_prime(state, bits, smoothness=16):
+    p = mpz(2)
+    p_factors = [p]
+    while p.bit_length() < bits - 2 * smoothness:
+        factor = get_prime(state, smoothness)
+        p_factors.append(factor)
+        p *= factor
+    bitcnt = (bits - p.bit_length()) // 2
+    while True:
+        prime1 = get_prime(state, bitcnt)
+        prime2 = get_prime(state, bitcnt)
+        tmpp = p * prime1 * prime2
+        if tmpp.bit_length() < bits:
+            bitcnt += 1
+            continue
+        if tmpp.bit_length() > bits:
+            bitcnt -= 1
+            continue
+        if is_prime(tmpp + 1):
+            p_factors.append(prime1)
+            p_factors.append(prime2)
+            p = tmpp + 1
+            break
+    p_factors.sort()
+    return (p, p_factors)
+
+if __name__ == "__main__":
+    q, q_factors = get_smooth_prime(STATE, 1500, 17)
